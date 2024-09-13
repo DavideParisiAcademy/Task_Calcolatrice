cb21e7fc19c9bd3a9a27e041f4a1a203d989dc6f
DavideParisiAcademy <d.parisi@iemmeconsulting.it>
2024-09-13 18:02:14 +0200

    version 1.0

---
+++ Task_Calcolatrice/Classes/Calcolatrice.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Calcolatrice.Classes
+{
+    internal class Calcolatrice
+    {
+
+        //COSTRUTTORE DEFAULT
+
+        
+
+        //METHODS
+
+        //1) SOMMA
+        
+        public void Somma()
+        {
+            Console.WriteLine("Inserisci due valori da Sommare");
+            string? x = Console.ReadLine();
+            string? y = Console.ReadLine();
+            double x1 = Convert.ToDouble(x);
+            double y1 = Convert.ToDouble(y);
+            Console.WriteLine($"Risultato: {x1 + y1}");
+        }
+        public void Differenza()
+        {
+            Console.WriteLine("Inserisci due valori da Sottrarre");
+            string? x = Console.ReadLine();
+            string? y = Console.ReadLine();
+            double x1 = Convert.ToDouble(x);
+            double y1 = Convert.ToDouble(y);
+            Console.WriteLine($"Risultato: {x1 - y1}");
+        }
+        public void Prodotto()
+        {
+            Console.WriteLine("Inserisci due valori da Moltiplicare");
+            string? x = Console.ReadLine();
+            string? y = Console.ReadLine();
+            double x1 = Convert.ToDouble(x);
+            double y1 = Convert.ToDouble(y);
+            Console.WriteLine($"Risultato: {x1 * y1}");
+        }
+        public void Quoziente()
+        {
+            Console.WriteLine("Inserisci due valori da Dividere");
+            string? x = Console.ReadLine();
+            string? y = Console.ReadLine();
+            double x1 = Convert.ToDouble(x);
+            double y1 = Convert.ToDouble(y);
+            if (y1 != 0)
+                Console.WriteLine($"Risultato: {x1 / y1}");
+            else Console.WriteLine("Errore: Divisione per 0");
+        }
+        public void Potenza()
+        {
+            Console.WriteLine("Inserisci la base: ");
+            string? x = Console.ReadLine();
+            Console.WriteLine("Inserisci l'esponente: ");
+            string? y = Console.ReadLine();
+            double y1 = Convert.ToDouble(y);
+            double x1 = Convert.ToDouble(x);;
+            
+            Console.WriteLine($"Risultato: {Math.Pow(x1, y1)}");
+        }
+        public void Radice()
+        {
+            Console.WriteLine("Inserisci il radicando: ");
+            string? x = Console.ReadLine();
+            double x1 = Convert.ToDouble(x);
+            if (x1 >= 0)
+                Console.WriteLine($"Risultato: {Math.Sqrt(x1)}");
+            else
+                Console.WriteLine("Errore: errore di inserimento");
+        }
+
+        public void SommaComplessa()
+        {
+            Console.WriteLine("Quanti valori vuoi inserire? ");
+            string? d = Console.ReadLine();
+            int dimensione = Convert.ToInt32(d);
+            if (dimensione > 0)
+            {
+                double[] sommatoria = new double[dimensione];
+                double s = 0;
+                //Inserimento
+                for (int i = 0; i < dimensione; i++)
+                {
+                    Console.WriteLine("Inserisci valore: ");
+                    double valore = Convert.ToDouble(Console.ReadLine());
+                    sommatoria[i] = valore;
+                    s = s + valore;
+                }
+                Console.WriteLine($"Risultato:{s}");
+            }
+            else { 
+                Console.WriteLine("Errore di inserimento: Dimensione negativa");
+            }
+        }
+        public void DifferenzaComplessa()
+        {
+            Console.WriteLine("Quanti valori vuoi inserire? ");
+            string? d = Console.ReadLine();
+            int dimensione = Convert.ToInt32(d);
+            double[] differenza = new double[dimensione];
+            double p;
+            double s = 0;
+
+            //Inserimento
+            for (int i = 0; i < dimensione; i++)
+            {
+                Console.WriteLine("Inserisci valore: ");
+                double valore = Convert.ToDouble(Console.ReadLine());
+                differenza[i] = valore;
+                      
+            } 
+            
+            Console.WriteLine($"Risultato:{s}"); 
+        }
+
+        public void ProdottoComplesso()
+        {
+            Console.WriteLine("Quanti valori vuoi inserire? ");
+            string? d = Console.ReadLine();
+            int dimensione = Convert.ToInt32(d);
+            if (dimensione > 0)
+            {
+                double[] Produttoria = new double[dimensione];
+                double s = 1;
+                //Inserimento
+                for (int i = 0; i < dimensione; i++)
+                {
+                    Console.WriteLine("Inserisci valore: ");
+                    double valore = Convert.ToDouble(Console.ReadLine());
+                    Produttoria[i] = valore;
+                    s = s * valore;
+                }
+                Console.WriteLine($"Risultato:{s}");
+            }
+        }
+
+        public void QuozienteComlplesso()
+        {
+            Console.WriteLine("Quanti valori vuoi inserire? ");
+            string? d = Console.ReadLine();
+            int dimensione = Convert.ToInt32(d);
+            double[] Quoziente = new double[dimensione];
+
+            if (dimensione <= 0)
+            {
+                Console.WriteLine("Errore di inserimento: Dimensione negativa");
+            }
+            else
+            {
+                //Inserimento
+                for (int i = 0; i < dimensione; i++)
+                {
+
+                    Console.WriteLine("Inserisci valore: ");
+                    double valore = Convert.ToDouble(Console.ReadLine());
+                    Quoziente[i] = valore;
+                }
+                for (int j = 0; j < dimensione; j++)
+                {
+                    double s = Quoziente[j];
+                    Console.WriteLine(Quoziente[j]);
+                    //if (Quoziente[j + 1] != 0) {
+                    //    s = s / Quoziente[j + 1];
+                    //}
+                    //Console.WriteLine($"Risultato: {s}");         
+                }
+            }
+        }
+
+        
+
+        
+
+    }
+}
---
+++ Task_Calcolatrice/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Reflection;
+using Task_Calcolatrice.Classes;
+
+namespace Task_Calcolatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcolatrice calcolatrice = new Calcolatrice();
+            bool ciclo = true;
+
+            while (ciclo)
+            {
+
+                Console.WriteLine("Scegli un'operazione:\n1)Somma\n2)Differenza\n3)Prodotto\n4)Quoziente\n5)Potenza\n6)Radice Quadrata");
+                Console.WriteLine("7)Operazioni a più cifre");
+                Console.WriteLine("Premere Q per chiudere il programma");
+
+                string? inputUtente = Console.ReadLine();
+                
+                if (inputUtente is not null)
+                {
+                    inputUtente = inputUtente.ToUpper();
+                }
+                    switch (inputUtente)
+                    {
+                        case "1":
+                            calcolatrice.Somma();
+                            break;
+                        case "2":
+                            calcolatrice.Differenza();
+                            break;
+                        case "3":
+                            calcolatrice.Prodotto();
+                            break;
+                        case "4":
+                            calcolatrice.Quoziente();
+                            break;
+                        case "5":
+                            calcolatrice.Potenza();
+                            break;
+                        case "6":
+                            calcolatrice.Radice();
+                            break;
+                        case "7":
+                            Console.WriteLine("Scegli un'operazione a più valori fra:\n1)Somma\n2)Differenza\n3)Prodotto\n4)Quoziente");
+                            string? inputUtente1 = Console.ReadLine();
+                            if (inputUtente1 is not null)
+                            {
+                                inputUtente1 = inputUtente1.ToUpper();
+                            }
+                            switch (inputUtente1) { 
+                            
+                                case "1":
+                                    calcolatrice.SommaComplessa();
+                                    break;
+                                case "2":
+                                    calcolatrice.DifferenzaComplessa();
+                                    break;
+                                case "3":
+                                    calcolatrice.ProdottoComplesso();
+                                    break;
+                                case "4":
+                                calcolatrice.DifferenzaComplessa();
+                                    break;                           
+                            }
+                            break;
+                        case "Q":
+                            ciclo = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("ERRORE");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
